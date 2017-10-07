ccb74fd0531658f32d52bd06d4ad0620ba20e804
Matthias Koch <ithrowexceptions@gmail.com>
2017-10-07 23:09:56 +0200

    WIP

---
+++ source/Nuke.Common/ProjectModel/Solution.cs
@@ -0,0 +1,29 @@
+﻿// Copyright Matthias Koch 2017.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Nuke.Common.ProjectModel
+{
+    [PublicAPI]
+    public class Solution
+    {
+        internal Solution (string path, IReadOnlyCollection<Project> projects)
+        {
+            Path = path;
+            Projects = projects;
+        }
+
+        public string Path { get; }
+        public IReadOnlyCollection<Project> Projects { get; }
+
+        public static implicit operator string (Solution solution)
+        {
+            return solution.Path;
+        }
+    }
+}
